281f7c0e731022de3c07ee8e4d3fdb5d017e874b
= <=>
2021-10-04 08:13:48 +0100

    Attack System Done

--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : Enemy
+public class Skeleton : Enemy, IDamagable
 {
+    public int Health { get; set; }
+
     public override void Init()
     {
         base.Init();
         target = pointB.position;
         speed = 0.5f;
+    }
+
+    public void Damage()
+    {
+
     }
 }
